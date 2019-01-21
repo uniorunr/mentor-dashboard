46fa27329182b18ea7f57e3337283851164a314e
uniorunr <gagael23@live.com>
2019-01-21 22:26:46 +0300

    feature: add dependencies and change config files to work with react libriary

--- rss-mentor-dashboard/webpack.config.js
+++ rss-mentor-dashboard/webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/app.js'],
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js',
+    sourceMapFilename: 'bundle.map',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
   },
   watch: true,
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: '#source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
