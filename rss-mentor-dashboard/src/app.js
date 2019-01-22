9f7dd35cf605f35d551cf8c511bb2570eecb7ad4
uniorunr <gagael23@live.com>
2019-01-23 01:23:45 +0300

    feature: add functions to create a final joined data file with all info about mentors

--- rss-mentor-dashboard/src/app.js
+++ rss-mentor-dashboard/src/app.js
@@ -35,16 +35,20 @@ const fieldMapping = {
 const getMentor = (sheet, currentRow) => {
   const name = sheet[fieldMapping.mentorGithub.name + currentRow].v;
   const surname = sheet[fieldMapping.mentorGithub.surname + currentRow].v;
-  const worker = {
+  const github = sheet[fieldMapping.mentorGithub.github + currentRow].v;
+
+  const mentor = {
     name,
     surname,
     fullName: `${name.trim().toLowerCase()} ${surname.trim().toLowerCase()}`,
     city: sheet[fieldMapping.mentorGithub.city + currentRow].v,
     count: sheet[fieldMapping.mentorGithub.count + currentRow].v,
-    github: sheet[fieldMapping.mentorGithub.github + currentRow].v,
+    github,
+    githubUsername: github.replace(/^.*:\/\/.*\//, '').replace('/', '').toLowerCase(),
+    students: [],
   };
 
-  return worker;
+  return mentor;
 };
 
 const getMentors = (sheet, max) => {
@@ -58,8 +62,43 @@ const getMentors = (sheet, max) => {
   return rows;
 };
 
-const pairs = getMentors(mentorGithub, getNumberOfRows(mentorGithub));
-const pairsJSON = JSON.stringify(pairs, 0, 2);
+const mentors = getMentors(mentorGithub, getNumberOfRows(mentorGithub));
+
+const studentGithub = mentorStudentPairsWB.Sheets.pairs;
+
+const getPair = (sheet, currentRow) => {
+  const pair = {
+    interviewer: sheet[fieldMapping.pairs.interviewer + currentRow].v.trim().toLowerCase(),
+    student: sheet[fieldMapping.pairs.student + currentRow].v,
+  };
+
+  return pair;
+};
+
+const getPairs = (sheet, max) => {
+  let curRow = 2;
+  const rows = [];
+  while (curRow <= max) {
+    rows.push(getPair(sheet, curRow));
+    curRow += 1;
+  }
+
+  return rows;
+};
+
+const pairs = getPairs(studentGithub, getNumberOfRows(studentGithub));
+
+pairs.forEach((pair) => {
+  const mentor = mentors.find(mntr => mntr.fullName === pair.interviewer);
+
+  if (!mentor) return;
+
+  mentor.students.push(String(pair.student));
+});
+
+const mentorStudentPairs = mentors.filter(r => r);
+
+const pairsJSON = JSON.stringify(mentorStudentPairs, 0, 2);
 
 fs.writeFile('./data/final/pairs.json', pairsJSON, 'utf8', () => {
   console.log('Writing is done!');
