42cf4b1f337103a2e961320e34fd056cdb430a9a
uniorunr <gagael23@live.com>
2019-01-30 23:19:43 +0300

    feature: create main css file, add google font to it

--- rss-mentor-dashboard/src/app.jsx
+++ rss-mentor-dashboard/src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 import NavBar from './components/NavBar/NavBar';
 
 ReactDOM.render(<NavBar />, document.getElementById('root'));
