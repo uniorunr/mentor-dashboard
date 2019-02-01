29d5d2b4e6d683f4fb1bb56c1a76a43369c44450
uniorunr <gagael23@live.com>
2019-02-02 00:49:56 +0300

    feature: make function to parse JSON and combine data for dashboard

--- rss-mentor-dashboard/src/utils/parseJSON.js
+++ rss-mentor-dashboard/src/utils/parseJSON.js
@@ -2,13 +2,13 @@ import dataJSON from '../../data/data.json';
 
 const data = JSON.parse(JSON.stringify(dataJSON));
 
-const mentors = [];
+const mentorsList = [];
 
 data.mentors.forEach((element) => {
-  mentors.push({ value: element.githubUsername, label: element.githubUsername });
+  mentorsList.push({ value: element.githubUsername, label: element.githubUsername });
 });
 
-mentors.sort((a, b) => {
+mentorsList.sort((a, b) => {
   const nameA = a.value;
   const nameB = b.value;
   if (nameA < nameB) {
@@ -21,4 +21,47 @@ mentors.sort((a, b) => {
   return 0;
 });
 
-export default mentors;
+const studentsList = [];
+
+data.mentors[1].students.forEach((element) => {
+  studentsList.push(element.github);
+});
+
+const tasksList = [];
+
+data.tasks.forEach((element) => {
+  tasksList.push(element.taskName);
+});
+
+const dataForDashboard = [];
+
+const getDataByMentor = (mentor, dataObj) => {
+  const currMentor = dataObj.mentors.filter(ment => ment.githubUsername === mentor)[0];
+  const finalData = [];
+
+  currMentor.students[0].tasks.forEach((task) => {
+    finalData.push({ task: task.taskName });
+  });
+
+  finalData.forEach((task, i) => {
+    currMentor.students.forEach((student) => {
+      Object.defineProperty(task, student.github, {
+        value: student.tasks[i].status,
+        configurable: true,
+        enumerable: true,
+      });
+    });
+  });
+
+  return finalData;
+};
+
+const test = getDataByMentor('alex-zayats', data);
+
+data.mentors[1].students.forEach((element) => {
+  dataForDashboard.push(element.github);
+});
+
+export {
+  mentorsList, studentsList, tasksList, test,
+};
