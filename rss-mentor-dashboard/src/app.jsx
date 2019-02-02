d29b6703cbfddf5b1b2b8ec4778952f6d5f819f0
uniorunr <gagael23@live.com>
2019-02-03 02:06:24 +0300

    feature: move all rendering to App.jsx

--- rss-mentor-dashboard/src/app.jsx
+++ rss-mentor-dashboard/src/app.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Table from './components/Dashboard/Dashboard';
 
-ReactDOM.render(<NavBar />, document.getElementById('root'));
+class App extends Component {
+  state = {
+    mentor: null,
+  };
+
+handleInput = (mentorInput) => {
+  this.setState({
+    mentor: mentorInput,
+  });
+}
+
+render() {
+  const { mentor } = this.state;
+  return (
+    <Fragment>
+      <NavBar handleInput={this.handleInput} />
+      {mentor ? <Table mentor={mentor} /> : null}
+    </Fragment>
+  );
+}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
