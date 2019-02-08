b369ab38a649ef1340af96132c7cdb2d1b9d0314
uniorunr <gagael23@live.com>
2019-02-09 00:23:43 +0300

    test: add firebase mocking, update tests

--- rss-mentor-dashboard/src/components/NavBar/NavBar.test.jsx
+++ rss-mentor-dashboard/src/components/NavBar/NavBar.test.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import * as firebase from 'firebase';
 import NavBar from './NavBar';
+
+const onAuthStateChanged = jest.fn();
+const signInWithPopup = jest.fn(() => Promise.resolve('result of signInWithRedirect'));
+
+jest.spyOn(firebase, 'initializeApp')
+  .mockImplementation(() => ({
+    auth: () => ({
+      currentUser: {},
+      signInWithPopup,
+    }),
+  }));
+
+jest.spyOn(firebase, 'auth').mockImplementation(() => ({
+  onAuthStateChanged,
+  currentUser: {
+    displayName: 'testDisplayName',
+    email: 'test@test.com',
+    emailVerified: true,
+  },
+}));
 
 const handleInput = (mentorInput) => {
   this.setState({
